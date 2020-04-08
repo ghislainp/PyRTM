a77c7bb5d0a9a0c711e419cbb1c7c3722053533d
Ghislain Picard <ghislain.picard@univ-grenoble-alpes.fr>
2020-04-08 21:36:35 +0200

    reactivate smarts test

--- test/test_basic.py
+++ test/test_basic.py
@@ -3,10 +3,10 @@ import atmosrt
 
 
 
-# def test_smarts():
-# 	# very basic test
-# 	moderate_model = atmosrt.SMARTS(atmosrt.settings.pollution['moderate'])
-# 	print(moderate_model.spectrum())
+ def test_smarts():
+ 	# very basic test
+ 	moderate_model = atmosrt.SMARTS(atmosrt.settings.pollution['moderate'])
+ 	print(moderate_model.spectrum())
 
 
 def test_sbdart():
