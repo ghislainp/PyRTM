27c1e7a70f9d5f6271435b175a5e051d5c7b817c
Ghislain Picard <ghislain.picard@univ-grenoble-alpes.fr>
2020-04-09 08:16:54 +0200

    indentation error

--- test/test_basic.py
+++ test/test_basic.py
@@ -3,14 +3,14 @@ import atmosrt
 
 
 
- def test_smarts():
- 	# very basic test
- 	moderate_model = atmosrt.SMARTS(atmosrt.settings.pollution['moderate'])
- 	print(moderate_model.spectrum())
+def test_smarts():
+    # very basic test
+    moderate_model = atmosrt.SMARTS(atmosrt.settings.pollution['moderate'])
+    print(moderate_model.spectrum())
 
 
 def test_sbdart():
-	# very basic test
-	moderate_model = atmosrt.SBdart(atmosrt.settings.pollution['moderate'])
+    # very basic test
+    moderate_model = atmosrt.SBdart(atmosrt.settings.pollution['moderate'])
 
-	print(moderate_model.spectrum())
+    print(moderate_model.spectrum())
